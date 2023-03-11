ee2a6b9e9f259a8f45411bee010109f763c4fa21
kolynzb <collinsbenda360@gmail.com>
2023-03-12 02:49:14 +0300

    fix: correct modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport/dist';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-  providers: [AuthService],
-  imports: [UserModule],
   controllers: [AuthController],
+  imports: [UserModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
