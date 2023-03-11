9457f882740ae1454c8189407445e05d5b6c08aa
kolynzb <collinsbenda360@gmail.com>
2023-03-12 00:36:16 +0300

    feat: create user services

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,33 @@
 import { Injectable } from '@nestjs/common';
 // import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entity/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  create(createUserDto: CreateUserDto): Promise<User> {
+    return this.userRepository.save(createUserDto);
+  }
+  getAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  getOne(id: number): Promise<User> {
+    return this.userRepository.findOne({ where: { id } });
+  }
+
   update(updateUserDto: UpdateUserDto, id: number) {
-    return {
-      email: updateUserDto.email,
-      id,
-    };
+    return this.userRepository.update(id, updateUserDto);
+  }
+
+  remove(id: number) {
+    return this.userRepository.delete(id);
   }
 }
