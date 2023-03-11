bcb7858c417b068af92f5625803183a7e293608f
kolynzb <collinsbenda360@gmail.com>
2023-03-11 22:35:22 +0300

    feat: create home  module

--- src/home/home.module.ts
+++ src/home/home.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HomeController } from './home.controller';
+import { HomeService } from './home.service';
 
 @Module({
-  controllers: [HomeController]
+  controllers: [HomeController],
+  providers: [HomeService],
 })
 export class HomeModule {}
