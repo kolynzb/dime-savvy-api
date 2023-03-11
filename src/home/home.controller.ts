6f5e0a21ba035af9d2a3737a4cfa7fc856a41187
kolynzb <collinsbenda360@gmail.com>
2023-03-11 22:33:26 +0300

    feat: create home controller

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
+import { HomeService } from './home.service';
 
-@Controller('home')
-export class HomeController {}
+@Controller()
+export class HomeController {
+  constructor(private service: HomeService) {}
+
+  @Get('/')
+  @Render('index')
+  home() {
+    return {
+      viewData: this.service.AppInfo(),
+    };
+  }
+}
