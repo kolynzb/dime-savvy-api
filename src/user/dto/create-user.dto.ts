0e74555352351d93fbc0c2480aa263e72a97a04e
kolynzb <collinsbenda360@gmail.com>
2023-03-12 00:36:31 +0300

    refactor: update create user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,9 +1,12 @@
 import { IsString, IsEmail } from 'class-validator';
 
-export class UpdateUserDto {
+export class CreateUserDto {
   @IsString()
   name: string;
 
   @IsEmail()
   email: string;
+
+  @IsString()
+  password: string;
 }
