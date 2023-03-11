861c7ee72959ce1d2df10e18efaadbe785679329
kolynzb <collinsbenda360@gmail.com>
2023-03-12 02:48:57 +0300

    feat: add email passeord validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+
+    if (user && user.password === password) {
+      return user;
+    }
+
+    return null;
+  }
+}
