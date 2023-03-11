44387dea84879f63c5b90a11fab0cc46e71ba32f
kolynzb <collinsbenda360@gmail.com>
2023-03-12 00:35:33 +0300

    feat: user crud

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, ParseIntPipe, Patch } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { Body, Param } from '@nestjs/common/decorators';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserService } from './user.service';
@@ -6,11 +13,32 @@ import { UserService } from './user.service';
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @Post('')
+  createUser(@Body() updateUserDto: UpdateUserDto) {
+    return this.userService.create(updateUserDto);
+  }
+
+  @Get('')
+  getUsers() {
+    return this.userService.getAll();
+  }
+
+  @Get(':id')
+  getUser(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.getOne(id);
+  }
+
   @Patch(':id')
   updateUser(
     @Body() updateUserDto: UpdateUserDto,
     @Param('id', ParseIntPipe) id: number,
   ) {
     return this.userService.update(updateUserDto, id);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.getOne(id);
   }
 }
