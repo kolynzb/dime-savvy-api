716e70deaded0571ab21bd73aeebb311e64cfabd
kolynzb <collinsbenda360@gmail.com>
2023-03-11 20:27:27 +0300

    feat: connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import {} from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
+import { type } from 'os';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'i4iridium14C++',
+      database: 'dimesavvy',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
