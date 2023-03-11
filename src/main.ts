a3f3bfa9ad600a863a21bc959456d1560051173f
kolynzb <collinsbenda360@gmail.com>
2023-03-11 22:32:54 +0300

    feat: add hbs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import * as hbs from 'hbs';
+import * as hbsUtils from 'hbs-utils';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public')); // public folder used for asset storage
+  app.setBaseViewsDir(join(__dirname, '..', 'views')); //  specify for hbs to know where the views are
+  hbs.registerPartials(join(__dirname, '..', 'views/layouts')); // storing Handlebars Partials
+  hbsUtils(hbs).registerWatchedPartials(join(__dirname, '..', 'views/layouts')); //watch the views/layouts folder for changes
+  app.setViewEngine('hbs');
   await app.listen(3000);
 }
 bootstrap();
