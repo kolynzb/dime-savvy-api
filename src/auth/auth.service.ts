251012c5de34cfe8d8983f3e338a81854b6e7653
kolynzb <collinsbenda360@gmail.com>
2023-03-12 03:19:28 +0300

    feat: add jwt strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
-import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string) {
     const user = await this.userService.findByEmail(email);
 
     if (user && user.password === password) {
-      return user;
+      const { password, ...result } = user;
+      return result;
     }
 
     return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
