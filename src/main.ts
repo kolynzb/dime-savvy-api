60c27f9237df35016503487c09146c6675644ad1
kolynzb <collinsbenda360@gmail.com>
2023-03-12 20:47:28 +0300

    feat add config service

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,15 @@ import { join } from 'path';
 import * as hbs from 'hbs';
 import * as hbsUtils from 'hbs-utils';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const configService = app.get(ConfigService);
+  app.setGlobalPrefix(configService.get('app.apiPrefix'), {
+    exclude: ['/'],
+  });
   app.useStaticAssets(join(__dirname, '..', 'public')); // public folder used for asset storage
   app.setBaseViewsDir(join(__dirname, '..', 'views')); //  specify for hbs to know where the views are
   hbs.registerPartials(join(__dirname, '..', 'views/layouts')); // storing Handlebars Partials
@@ -15,6 +21,6 @@ async function bootstrap() {
   app.setViewEngine('hbs');
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3000);
+  await app.listen(configService.get('app.port'));
 }
 bootstrap();
