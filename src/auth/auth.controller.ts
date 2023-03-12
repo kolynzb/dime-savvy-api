c5f8fbf171789d702bdf5c205074fdde34c3ac2a
kolynzb <collinsbenda360@gmail.com>
2023-03-12 03:20:10 +0300

    feat: add auth controller jwt guear

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,23 @@
 import { Controller, Request, UseGuards } from '@nestjs/common';
-import { Post } from '@nestjs/common/decorators';
-import { AuthGuard } from '@nestjs/passport/dist/auth.guard';
+import { Get, Post } from '@nestjs/common/decorators';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('/login')
   async login(@Request() req: any) {
+    return this.authService.login(req.user);
+  }
+
+  //protected route
+  @UseGuards(JwtAuthGuard)
+  @Get('/profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 }
