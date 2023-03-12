e38c0dd4f36bfb9f4790450d61241afd218f1db2
kolynzb <collinsbenda360@gmail.com>
2023-03-12 20:47:07 +0300

    feat:add configurations for typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,29 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm/dist';
 import { HomeModule } from './home/home.module';
 import { UserModule } from './user/user.module';
-import { User } from './user/entity/user.entity';
 import { AuthModule } from './auth/auth.module';
 import { MailModule } from './mail/mail.module';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+import authConfig from './config/auth.config';
+import appConfig from './config/app.config';
+import mailConfig from './config/mail.config';
+import googleConfig from './config/google.config';
+import { TypeOrmConfigService } from './database/typeorm-config.service';
+import { DataSource } from 'typeorm';
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '',
-      database: 'dimesavvy',
-      entities: [User],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, authConfig, appConfig, mailConfig, googleConfig],
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+      dataSourceFactory: async (options) => {
+        const dataSource = await new DataSource(options).initialize();
+        return dataSource;
+      },
     }),
     HomeModule,
     UserModule,
