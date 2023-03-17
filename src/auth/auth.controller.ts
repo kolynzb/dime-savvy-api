022a48165630b24cba425881fa0fd616cb4b36fb
kolynzb <collinsbenda360@gmail.com>
2023-03-17 21:39:51 +0300

    feat(utils): update guard imports

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,8 @@
 import { Controller, Request, UseGuards } from '@nestjs/common';
 import { Get, Post } from '@nestjs/common/decorators';
 import { AuthService } from './auth.service';
-import { JwtAuthGuard } from './jwt-auth.guard';
-import { LocalAuthGuard } from './local-auth.guard';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
