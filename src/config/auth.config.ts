facf32a59e76daad6e914faf9cbd17f2d52d3f3a
kolynzb <collinsbenda360@gmail.com>
2023-03-17 21:16:01 +0300

    feat: add default expiration

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -2,5 +2,5 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('auth', () => ({
   secret: process.env.AUTH_JWT_SECRET,
-  expires: process.env.AUTH_JWT_TOKEN_EXPIRES_IN,
+  expires: process.env.AUTH_JWT_TOKEN_EXPIRES_IN || '60s',
 }));
