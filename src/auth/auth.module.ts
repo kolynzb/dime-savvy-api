7dba4d376924e076837f7ef5d7c316fc6f164be0
kolynzb <collinsbenda360@gmail.com>
2023-03-17 21:15:26 +0300

    feat: add mail modules - Update JWTStrategy module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,20 +3,31 @@ import { PassportModule } from '@nestjs/passport/dist';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { LocalStrategy } from './local.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
-import { JwtStrategy } from './jwt.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MailModule } from 'src/mail/mail.module';
+import { AnonymousStrategy } from './strategies/anonymous.strategy';
+
 @Module({
-  controllers: [AuthController],
   imports: [
     UserModule,
     PassportModule,
-    JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+    MailModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('auth.secret'),
+        signOptions: {
+          expiresIn: configService.get('auth.expires'),
+        },
+      }),
     }),
   ],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, JwtStrategy, AnonymousStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
